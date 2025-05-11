8054b58c300ca2c55d7ce2df869d936ef23d32b0
Ruslan Frolov <Ezpath@yandex.ru>
2025-05-11 12:16:27 +0300

    refactor: remove dead code

--- src/main/java/ru/carbohz/intershop/repository/ItemRepository.java
+++ src/main/java/ru/carbohz/intershop/repository/ItemRepository.java
@@ -2,9 +2,6 @@ package ru.carbohz.intershop.repository;
 
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Modifying;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 import ru.carbohz.intershop.model.Item;
 
@@ -13,22 +10,4 @@ import java.util.List;
 @Repository
 public interface ItemRepository extends JpaRepository<Item, Long> {
     List<Item> findByTitleContainingOrDescriptionContainingAllIgnoreCase(String title, String description, Pageable pageable);
-
-//    @Modifying
-//    @Query("UPDATE Item i SET i.count = i.count + 1 WHERE i.id = :id")
-//    void increaseCount(@Param("id") Long id);
-//
-//    @Modifying
-//    @Query("UPDATE Item i SET i.count = i.count - 1 WHERE i.id = :id")
-//    void decreaseCount(@Param("id") Long id);
-//
-//    @Modifying
-//    @Query("UPDATE Item i SET i.count = 0 WHERE i.id = :id")
-//    void resetCount(@Param("id") Long id);
-//
-//    @Modifying
-//    @Query("UPDATE Item i SET i.count = 0")
-//    void resetCountForAll();
-//
-//    List<Item> findAllByCountIsGreaterThan(long count);
 }

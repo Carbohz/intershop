b96b13a64263771f321ee6bc5863c86a649b7d1e
Ruslan Frolov <Ezpath@yandex.ru>
2025-07-20 15:55:34 +0300

    fix: HomeController

--- src/main/java/ru/carbohz/intershop/controller/HomeController.java
+++ src/main/java/ru/carbohz/intershop/controller/HomeController.java
@@ -2,11 +2,17 @@ package ru.carbohz.intershop.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import reactor.core.publisher.Mono;
 
 @Controller
 public class HomeController {
+//    @GetMapping
+//    public String home() {
+//        return "redirect:/main/items";
+//    }
+
     @GetMapping
-    public String home() {
-        return "redirect:/main/items";
+    public Mono<String> home() {
+        return Mono.just("redirect:/main/items");
     }
 }

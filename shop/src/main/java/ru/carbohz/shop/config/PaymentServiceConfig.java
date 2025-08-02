dcf7575a04cb495605610da5a10578c7e3fa1986
Ruslan Frolov <Ezpath@yandex.ru>
2025-08-02 13:43:35 +0300

    feat: make PaymentApi configurable

--- shop/src/main/java/ru/carbohz/shop/config/PaymentServiceConfig.java
+++ shop/src/main/java/ru/carbohz/shop/config/PaymentServiceConfig.java
@@ -1,5 +1,6 @@
 package ru.carbohz.shop.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import ru.carbohz.shop.ApiClient;
@@ -8,9 +9,16 @@ import ru.carbohz.shop.api.PaymentApi;
 @Configuration
 public class PaymentServiceConfig {
 
+    @Value("${payment.service.host:localhost}")
+    private String host;
+
+    @Value("${payment.service.port:8081}")
+    private String port;
+
     @Bean
     public PaymentApi paymentApi() {
-        final ApiClient apiClient = new ApiClient().setBasePath("http://localhost:8081/");
+        final String baseUrl = String.format("http://%s:%s", host, port);
+        final ApiClient apiClient = new ApiClient().setBasePath(baseUrl);
         return new PaymentApi(apiClient);
     }
 }

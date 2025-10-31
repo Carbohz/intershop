24c402fe23343d71ab4c22d84a369d24666578b9
Ruslan Frolov <Ezpath@yandex.ru>
2025-11-01 01:32:00 +0300

    fix: last tests

--- shop/src/test/java/ru/carbohz/shop/ShopApplicationTests.java
+++ shop/src/test/java/ru/carbohz/shop/ShopApplicationTests.java
@@ -3,12 +3,21 @@ package ru.carbohz.shop;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.annotation.Import;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
+import ru.carbohz.shop.config.KeycloakTestcontainersConfiguration;
 import ru.carbohz.shop.config.PostgresTestcontainersConfiguration;
 import ru.carbohz.shop.config.RedisTestcontainersConfiguration;
 
-@Import({PostgresTestcontainersConfiguration.class, RedisTestcontainersConfiguration.class})
+@Import({PostgresTestcontainersConfiguration.class, RedisTestcontainersConfiguration.class, KeycloakTestcontainersConfiguration.class})
 @SpringBootTest
 class ShopApplicationTests {
+
+	@DynamicPropertySource
+	static void configureProperties(DynamicPropertyRegistry registry) {
+		registry.add("spring.security.oauth2.client.provider.keycloak.issuer-uri",
+				() -> "http://localhost:%d/realms/master".formatted(KeycloakTestcontainersConfiguration.keycloak.getMappedPort(8080)));
+	}
 
 	@Test
 	void contextLoads() {

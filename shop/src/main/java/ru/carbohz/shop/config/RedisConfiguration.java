5536d405ec880f18faa81bdb2fc31500b6ccab81
Ruslan Frolov <Ezpath@yandex.ru>
2025-10-31 15:40:01 +0300

    feat: remove user from cache

--- shop/src/main/java/ru/carbohz/shop/config/RedisConfiguration.java
+++ shop/src/main/java/ru/carbohz/shop/config/RedisConfiguration.java
@@ -10,7 +10,6 @@ import org.springframework.data.redis.serializer.Jackson2JsonRedisSerializer;
 import org.springframework.data.redis.serializer.RedisSerializationContext;
 import ru.carbohz.shop.dto.ItemDto;
 import ru.carbohz.shop.dto.PageableItemsDto;
-import ru.carbohz.shop.model.User;
 
 import java.time.Duration;
 import java.time.temporal.ChronoUnit;
@@ -45,16 +44,6 @@ public class RedisConfiguration {
                                 .serializeValuesWith(
                                         RedisSerializationContext.SerializationPair.fromSerializer(
                                                 new Jackson2JsonRedisSerializer<>(ItemDto.class)
-                                        )
-                                )
-                )
-                .withCacheConfiguration(
-                        "users",
-                        RedisCacheConfiguration.defaultCacheConfig()
-                                .entryTtl(Duration.of(1, ChronoUnit.MINUTES)) // TODO
-                                .serializeValuesWith(
-                                        RedisSerializationContext.SerializationPair.fromSerializer(
-                                                new Jackson2JsonRedisSerializer<>(User.class) // TODO UserDto
                                         )
                                 )
                 );
